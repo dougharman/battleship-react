ec0425c74afa003e064c763be3c7a9450519389f
Doug Harman <dharman@4harman.com>
2018-08-26 20:20:17 -0500

    INITIAL COMMIT - started project with create-react-app, added web3@1.0.0-beta.26 (using yarn), and truffle-hdwallet-provider, then built base react project

---
+++ src/App.js
@@ -0,0 +1,168 @@
+import React, { Component } from 'react';
+import './App.css';
+import web3 from './web3';
+import battleship from './battleship';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      owner: '',
+      player1: '',
+      player2: '',
+      p1NumberOfGuesses: '',
+      p2NumberOfGuesses: '',
+      p1NumberOfHits: '',
+      p2NumberOfHits: '',
+      p1DiscoveredPositions: '',
+      p2DiscoveredPositions: '',
+      balance: '',
+      value: '',
+      message: '',
+      errorMessage: '',
+      loading: false
+    };
+  }
+
+
+  async componentDidMount() {
+    const accounts = await web3.eth.getAccounts();
+    const owner = await battleship.methods.owner().call();
+    const player1 = await battleship.methods.getPlayer1().call();
+    const player2 = await battleship.methods.getPlayer2().call();
+    const balance = await web3.eth.getBalance(battleship.options.address);
+    const p1NumberOfGuesses = await battleship.methods.getLengthP1().call();
+    const p2NumberOfGuesses = await battleship.methods.getLengthP2().call();
+    const p1NumberOfHits = await battleship.methods.getLengthHitsP1().call();
+    const p2NumberOfHits = await battleship.methods.getLengthHitsP2().call();
+    const p1PositionsDiscovered = await battleship.methods.getP1Hits().call();
+    const p2PositionsDiscovered = await battleship.methods.getP2Hits().call();
+    const battleshipAddress = await battleship.options.address
+    this.setState({ owner, player1, player2, balance, p1NumberOfGuesses, p2NumberOfGuesses, 
+      p1NumberOfHits, p2NumberOfHits, p1PositionsDiscovered, p2PositionsDiscovered });
+
+  }
+
+  // ================================================================================
+  // ACTION: START GAME (Player 1)
+  // ================================================================================
+
+  onSubmit = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    const player1 = accounts[0];
+    console.log(player1);
+    // if (!player1) {
+    //   alert('MetaMask is not enabled!');
+    // }
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await battleship.methods.startGame().send({ 
+      value: web3.utils.toWei(this.state.value, 'ether'),
+      from: web3.eth.accounts[1],
+      to: web3.eth.accounts[0],    // owner holds the funds
+      gas: '1000000' 
+    });
+
+    this.setState({ message: 'You have been entered!' });
+  };
+
+  // ================================================================================
+  // ACTION: ACCEPT CHALLENGE (Player 2)
+  // ================================================================================
+
+  // onSubmit = async event => {
+  //   event.preventDefault();
+
+  //   const accounts = await web3.eth.getAccounts();
+  //   const player2 = accounts[1];
+  //   console.log(player2);
+  //   // if (!player2) {
+  //   //   alert('MetaMask is not enabled!');
+  //   // }
+
+  //   this.setState({ message: 'Waiting on transaction success...' });
+
+  //   await battleship.methods.acceptChallenge().send({ 
+  //     guess: 'this.state.value',
+  //     value: web3.utils.toWei(this.state.value, 'ether'),
+  //     from: web3.eth.accounts[2],
+  //     to: web3.eth.accounts[0],     // owner holds the funds
+  //     gas: '1000000' 
+  //   });
+  
+  //   this.setState({ message: 'You have been entered!' });
+  // };
+
+  render() {
+    return (
+      <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Battleship Game</h1>
+        </header>
+      </div>
+      <div>
+        <form onSubmit={this.onSubmit} >
+          <h2>START GAME</h2>
+          <h4>To start a game, please submit your address, board, and 5 ETH</h4>
+          <div>
+          <label>Entrance fee:</label>
+            <input
+              name="startGame"
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            /><br />
+          </div>
+          <button>Enter</button>
+        </form>
+      </div>
+      <div>
+        <ul>
+          <li>Owner:    {this.state.owner}</li>
+          <li>Player1:  {this.state.player1}</li> 
+          <li>Balance:  {this.state.balance}</li>  
+        </ul>
+      </div>
+      </React.Fragment> /* @dev added here in order to comment out following lines, if
+                                uncomment, delete. It's duplicated on line 161
+                        */
+      // <div>
+      //   <form onSubmit={this.onSubmit}>
+      //     <h2>ACCEPT CHALLENGE</h2>
+      //     <h4>To accept the challenge, please submit your address, board, guess, and 5 ETH</h4>
+      //     <div>
+      //       <label>Your guess: </label>
+      //       <input    
+      //         name="acceptChallenge"
+      //         guess={this.state.value}
+      //         onChange={event => this.setState({ guess: event.target.value })}
+      //       /><br />
+      //       <label>Entrance fee:</label>
+      //       <input
+      //         name="acceptChallenge"
+      //         value={this.state.value}
+      //         onChange={event => this.setState({ value: event.target.value })}
+      //       /><br />
+      //     </div>
+      //     <button>Enter</button>
+      //   </form>
+      // </div>
+      // <div>
+      //   <ul>
+      //     <li>Owner:    {this.state.owner}</li>
+      //     <li>Player1:  {this.state.player1}</li> 
+      //     <li>Player2:  {this.state.player2}</li>  
+      //     <li>Balance:  {this.state.balance}</li>  
+      //   </ul>
+      // </div>
+      // </React.Fragment>
+    );
+  }
+}
+
+export default App;
+
---
+++ src/battleship.js
@@ -0,0 +1,773 @@
+import web3 from './web3';
+
+const address = '0x347b13ef88236d353f90043b5c1a7a85443214a7';
+
+const abi = [
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "p1LogOfGuesses",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "badActor1",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "p2PositionsDiscovered",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "p1Hits",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "forfeit1",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "player2",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "p2Misses",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "turnCount",
+      "outputs": [
+        {
+          "name": "",
+          "type": "int256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "p1PositionsDiscovered",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "startTurn",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "p1Misses",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "stage",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "p1Position",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "player1",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "p2Hits",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "forfeit2",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "gameId",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "p2LogOfGuesses",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "p2Position",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "balance",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "badActor2",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "players",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "fallback"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "gameId",
+          "type": "uint256"
+        }
+      ],
+      "name": "LogStartOfGame",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "gameId",
+          "type": "uint256"
+        }
+      ],
+      "name": "LogAcceptTheChallenge",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "gameId",
+          "type": "uint256"
+        }
+      ],
+      "name": "LogTakeYourTurnAttacking",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "gameId",
+          "type": "uint256"
+        }
+      ],
+      "name": "LogGameIsOver",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "gameId",
+          "type": "uint256"
+        }
+      ],
+      "name": "LogDeclareTheWinner",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "gameId",
+          "type": "uint256"
+        }
+      ],
+      "name": "LogWithdrawTheMoney",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "previousOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipRenounced",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "increment",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getLengthP1",
+      "outputs": [
+        {
+          "name": "length",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getLengthP2",
+      "outputs": [
+        {
+          "name": "length",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getLengthHitsP1",
+      "outputs": [
+        {
+          "name": "length",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getLengthHitsP2",
+      "outputs": [
+        {
+          "name": "length",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "startGame",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "guess",
+          "type": "uint256"
+        }
+      ],
+      "name": "acceptChallenge",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "guess",
+          "type": "uint256"
+        },
+        {
+          "name": "result",
+          "type": "uint256"
+        }
+      ],
+      "name": "takeTurnsAttacking",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getP1LogOfGuesses",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256[]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getP2LogOfGuesses",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256[]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getP1Hits",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getP2Hits",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "gameOver",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "declareWinner",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "withdrawMoney",
+      "outputs": [],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "toggleContractActive",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "kill",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getContractAddress",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getContractBalance",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getPlayer1",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getPlayer2",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+];
+
+export default new web3.eth.Contract(abi, address);
